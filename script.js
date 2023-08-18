2625d9d85bac9f98660bc072c736039089f64eb2
markr155 <markricho155@gmail.com>
2023-08-18 17:21:19 +1000

    moving laptop

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+};
 
-    this.info = function() {
-        console.log(`${this.title} by ${author}, ${pages} pages long, has been read: ${read}`)
-    }
-    myLibrary.push(this);
+
+function addBookToLibrary() {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
+
+function displaysArray() {
+    while (table.childElementCount > 1){table.removeChild(table.lastChild)};
+    myLibrary.forEach(book => {
+        const newRow = document.createElement('tr');
+        for (key in book) {
+            const newCell = document.createElement('td');
+            newCell.textContent = book[key];
+            newRow.appendChild(newCell);
+        }
+        table.appendChild(newRow);
+    })
+}
+
+//element declaration
+const modalButton = document.querySelector('.newButton');
+const dialog = document.querySelector('.bookDialog');
+const table = document.querySelector('.library');
+
+modalButton.addEventListener('click', () => {dialog.showModal();});
 
 const hobbit = new Book('The Hobbit', 'JRR Tolkien', '290', 'yes');
 
@@ -18,5 +39,6 @@ const bible = new Book('The Bible', 'Matthew, Mark, Luke, John', 2000, 'no');
 
 const dictionary = new Book('Oxford Dictionary', 'Oxford', 3000, 'yes');
 
-const table = document.querySelector(".table");
-
+myLibrary.push(hobbit);
+myLibrary.push(bible);
+myLibrary.push(dictionary)
